ec621fc52c1a22f44e2b189346662dfdacc9140f
Bryan Stearns <bryanstearns@gmail.com>
2012-07-30 16:56:10 -0700

    Send more parameters into the new Linode's attributes
    (Mostly to give the box access to its private IP address)

--- lib/chef/knife/linode_server_create.rb
+++ lib/chef/knife/linode_server_create.rb
@@ -233,6 +233,14 @@ class Chef
         bootstrap.config[:template_file] = locate_config_value(:template_file)
         bootstrap.config[:environment] = config[:environment]
         bootstrap.config[:host_key_verify] = config[:host_key_verify]
+        bootstrap.config[:first_boot_attributes] = {
+          'linode_id' => server.id.to_s,
+          'datacenter_id' => locate_config_value(:linode_datacenter),
+          'linode_flavor_id' => locate_config_value(:linode_flavor),
+          'linode_image_id' => locate_config_value(:linode_image),
+          'linode_kernel_id' => locate_config_value(:linode_kernel),
+          'ip_addresses' => server.ips.map(&:ip)
+        }
         bootstrap
       end
 
